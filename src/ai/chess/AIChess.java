1e6f8252baf6f975115f6ea1a34bed1ac004a7d1
Omar Ashour <you@example.com>
2017-12-06 23:16:16 +0200

    GUI Panel Start

--- src/ai/chess/AIChess.java
+++ src/ai/chess/AIChess.java
@@ -16,6 +16,8 @@ public class AIChess {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GameBoard gb = new GameBoard();
+        gb.setVisible(true);
     }
     
 }
